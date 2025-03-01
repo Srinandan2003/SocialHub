87dcc960986e7b0ccc5fd12916d5d317c3f0f95c
Srinandan <srinandan2003@gmail.com>
2025-03-01 08:49:55 +0530

    Commited after shadcn setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,8 @@ function App() {
 
   return (
     <>
-     <div className="p-10">
-      <h1 className="text-3xl font-bold mb-4">ShadCN Accordion Test</h1>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Click to Expand</AccordionTrigger>
-          <AccordionContent>
-            This is the content inside the accordion.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
+
+  
     </>
   )
 }
