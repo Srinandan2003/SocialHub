9cf6b0d520d1e453e65fe851f31ac1bc1976da7f
Srinandan <srinandan2003@gmail.com>
2025-03-01 09:49:56 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,13 @@
-const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const connectDB = require("./src/config/db");
+const app = require("./app");
 
 dotenv.config(); // Load environment variables
 
-const app = express();
 const PORT = process.env.PORT || 5000;
- 
-// Middleware
-app.use(cors());
-app.use(express.json()); // Parse JSON data
 
 // Connect to MongoDB
 connectDB();
-
-// Test Route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 // Start Server
 app.listen(PORT, () => {
