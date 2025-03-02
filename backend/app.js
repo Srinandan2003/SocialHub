64799c50be60d25f0952850ba2b55ae36b33f4bb
jagannathking <sethij832@gmail.com>
2025-03-02 10:40:32 +0530

    remove category Schema

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 
-import categoryRoutes from "./src/routes/category.routes.js";
 import commentRoutes from "./src/routes/comment.routes.js";
 import userRoutes from "./src/routes/user.routes.js";
 import postRoutes from "./src/routes/post.routes.js";
@@ -11,7 +10,7 @@ const app = express();
 
 // Middleware
 // Middleware
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND_URI}));
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
 
@@ -19,11 +18,10 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
 // Routes
 app.use("/api/users", userRoutes); // User Authentication & Profile
 app.use("/api/posts", postRoutes); // Post CRUD + Like + Search
-app.use("/api/categories", categoryRoutes); // Category Management
 app.use("/api/comments", commentRoutes); // Comment CRUD
 
 // Test Route
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("API is running...");
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import  { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from './components/ui/accordion'
 function App() {
- 
+  
+  const [testData, setTestData] = useState('')
+  // const fetchJoke = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://official-joke-api.appspot.com/random_joke"
+  //     );
+  //     const data = await response.json();
+  //     setJoke(data);
+  //   } catch (error) {
+  //     console.error("Error fetching joke:", error);
+  //   }
+  // };
 
+  // useEffect(() => {
+  //   fetchJoke();
+  // }, [reload]);
+
+  const fetchTestApi = async() => {
+    try{
+      const response = await fetch('http://localhost:5000/test')
+      const data = await response.json();
+      
+      setTestData(data);
+  
+    }catch(error) {
+      console.log("Something went wrong while fetching test data", error);
+    }
+  }
+  
+  useEffect(() => {
+    fetchTestApi();
+  }, [])
   return (
     <>
-
-  
+      <h3>Test data</h3>
+      {
+        testData? <div>{testData}</div>: <div>Noting</div>
+      }
     </>
   )
 }
